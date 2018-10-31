62312589f9f86fe7982aadafb8bdfcf407eda207
macoshita <macoshita@gmail.com>
2018-10-31 23:45:36 +0900

    Remove VSCode settings

--- .config/Code/install-extensions.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# VSCode の拡張をインストールする
-# VSCode をインストール後、 Cmd+Shift+P -> "shell" と入力して、 PATH に code を追加
-# その後このスクリプトを実行することで拡張がインストールされる
-code --install-extension EditorConfig.EditorConfig
-code --install-extension TeddyDD.fish
-code --install-extension octref.vetur
-code --install-extension vscodevim.vim
